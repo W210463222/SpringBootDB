89c8cf9fa4ace7f2fc174d1f1c6e5a667f6c629e
W210463222 <115503484+W210463222@users.noreply.github.com>
2022-12-07 01:09:10 -0600

    Delete springboot directory

--- springboot/src/main/java/edu/hccs/springboot/SpringbootJdbcDemoApplication.java
+++
@@ -1,9 +0,0 @@
-package edu.hccs.springboot;
-
-@SpringBootApplication
-public class SpringbootJdbcDemoApplication {
-    public static void main(String[] args)
-    {
-        SpringApplication.run(SpringbootJdbcDemoApplication.class, args);
-    }
-}
