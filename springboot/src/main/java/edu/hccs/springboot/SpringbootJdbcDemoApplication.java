8c60739099d215770404ce6a8c3d8212a26010eb
W210463222 <115503484+W210463222@users.noreply.github.com>
2022-12-07 01:09:39 -0600

    Add files via upload

---
+++ springboot/src/main/java/edu/hccs/springboot/SpringbootJdbcDemoApplication.java
@@ -0,0 +1,9 @@
+package edu.hccs.springboot;
+
+@SpringBootApplication
+public class SpringbootJdbcDemoApplication {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringbootJdbcDemoApplication.class, args);
+    }
+}
